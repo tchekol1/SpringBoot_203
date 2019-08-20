3664548bba84adcef5c5784c0ef9f3069061951a
tchekol1 <tchekol1@umbc.edu>
2019-08-20 10:00:34 -0400

    ex203

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Employee {
+    private String Fname;
+    private String Lname;
+    private Long SSN;
+    private int DOB;
+
+    public String getFname() {
+        return Fname;
+    }
+
+    public void setFname(String fname) {
+        Fname = fname;
+    }
+
+    public String getLname() {
+        return Lname;
+    }
+
+    public void setLname(String lname) {
+        Lname = lname;
+    }
+
+    public Long getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(Long SSN) {
+        this.SSN = SSN;
+    }
+
+    public int getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(int DOB) {
+        this.DOB = DOB;
+    }
+}
